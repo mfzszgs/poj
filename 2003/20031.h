cd5b1fabc01f25009dbe83b3fd18f71a576b4c42
mfzszgs <su_wy@outlook.com>
2015-01-19 22:10:53 +0800

    a

--- 2003/20031.h
+++ 2003/20031.h
@@ -1,2 +1,46 @@
+template<typename T>
+class tree_node{
+	tree_node * first_child,next_sibling;
+	T data;
+};
 
-class
+template<typename T>
+class tree{
+public:
+	class iterator;
+	iterator begin();
+	iterator end();
+	void swap_data(iterator one,iterator two);
+	iterator erase();
+	iterator fire();
+	iterator hire();
+	void print();
+	class iterator{
+	public:
+		iterator();
+		iterator(tree_node nd);
+		tree_node node;
+		bool operator!=(iterator it);
+		bool operator==(iterator it);
+		iterator operator++();
+		iterator operator--();
+		iterator operator++(int);
+		iterator operator--(int);
+
+	};
+private:tree_node<T> head,feet;
+};
+
+template<typename T>
+tree<T>::iterator tree<T>::begin(){
+	return iterator(head->nextsibling);
+}
+template<typename T>
+tree<T>::iterator tree<T>::end(){
+	return iterator(feet);
+}
+template<typename T>
+void tree<T>::swap_data(iterator one,iterator two){
+	T tmp;
+	one
+}
